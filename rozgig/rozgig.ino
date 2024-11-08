1bc1d05e0eb57f9e3ba91af6a72b874bf1717745
Igor-ai-tech <igor.andreevihc@gmail.com>
2024-11-08 11:48:22 +0300

    ДЗ	Добавил коментарии и вывод в порт (где смог)

--- rozgig/rozgig.ino
+++ rozgig/rozgig.ino
@@ -2,13 +2,13 @@
 // Макросы #define не занимают памяти, так как просто заменяют значения в коде на этапе компиляции
 #define CONTROLLER_VERSION '1.0'
 
-#define ROZGIG_VENT_DELAY 10000        // Задержка включения перед розжигом
-#define ROZGIG_TRANS_DELAY_PERED 3000  // Задержка перед включением трансформатора перед зажиганием
-#define ROZGIG_TRANS_DELAY_POSLE 3000  // Задержка перед выключением трансформатора после зажигания
-#define ROZGIG_TIME_FLAME_CONTROL 3000
-#define OPERATIONAL_DELAY 30  // Операционная задержка для стабилизации после выполнения операций
-#define DP_READ_COUNT 3
-#define ROZGIG_ATTEMPT_COUNT 5
+#define ROZGIG_VENT_DELAY 10000         // Задержка включения перед розжигом
+#define ROZGIG_TRANS_DELAY_PERED 3000   // Задержка перед включением трансформатора перед зажиганием
+#define ROZGIG_TRANS_DELAY_POSLE 3000   // Задержка перед выключением трансформатора после зажигания
+#define ROZGIG_TIME_FLAME_CONTROL 3000  // Осуществляется проверка наличия пламени в режиме пуска котла
+#define OPERATIONAL_DELAY 30            // Операционная задержка для стабилизации после выполнения операций
+#define DP_READ_COUNT 3                 // Количество чтения датчика пламени (дребезг)
+#define ROZGIG_ATTEMPT_COUNT 5          // Количество циклов запуска перед выводом аварии
 
 // Определение пинов
 #define ROZGIG_TRANS_PIN 1  // Пин для управления трансформатором
@@ -18,10 +18,12 @@
 void setup() {
 
   Serial.begin(9600);
-  Serial.println('Kotel start. Version:' + CONTROLLER_VERSION);
-  pinMode(ROZGIG_TRANS_PIN, OUTPUT);    // Настраиваем пин розжиг-трансформатора как выходной.
-  digitalWrite(ROZGIG_TRANS_PIN, LOW);  // Устанавливаем начальное состояние пина розжига в LOW
+  Serial.println('Kotel start. Version:' + CONTROLLER_VERSION); // Вывод сообщения
+  pinMode(ROZGIG_TRANS_PIN, OUTPUT);                            // Настраиваем пин розжиг-трансформатора как выходной.
+  digitalWrite(ROZGIG_TRANS_PIN, LOW);                          // Устанавливаем начальное состояние пина розжига в LOW
   pinMode(DP_PIN, INPUT);
+
+  
 }
 
 void loop() {
@@ -29,19 +31,18 @@ void loop() {
 
 // Функция розжига
 void rozgig() {
-  bool flag = false;
-  for (byte i = 0; i < ROZGIG_ATTEMPT_COUNT; i++) {
+  bool flag = false;                                  // Флаг для отслеживания успешности розжига
+  for (byte i = 0; i < ROZGIG_ATTEMPT_COUNT; i++) {   // Пытаемся выполнить розжиг и + 1 если не получилось
     if (rozgig_attempt()) {
       flag = true;
       break;
     }
   }
-  if (flag){
+  if (flag) {
     //действия при успешном розжиге
   } else {
     //ействия при неудачном розжиге
-}
-
+  }
 }
 
 bool rozgig_attempt() {
@@ -87,10 +88,14 @@ void relay_switch(bool mode, byte relay_pin) {
 
 // Функция relay_on() вызывает relay_switch() с параметром true, что включает реле
 void relay_on(byte relay_pin) {
+Serial.println('relay_on() begin. relay_pin: ' + relay_pin);
   relay_switch(true, relay_pin);
+  Serial.println('relay_on() end.');
 }
 
 // Функция relay_off() вызывает relay_switch() с параметром false, что выключает реле
 void relay_off(byte relay_pin) {
+  Serial.println('relay_off() begin. relay_pin = ' + relay_pin);
   relay_switch(false, relay_pin);
+  Serial.println('relay_off() end.');
 }
